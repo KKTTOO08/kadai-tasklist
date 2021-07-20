a07c4d7d9092c35b7c1f75e40211f7075551603b
KKTTOO <hongtaishankou755@gmail.com>
2021-07-20 23:08:12 +0000

    課題提出_2nd

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.finf(params[:id])
+        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'タスクは正常に削除されました'
