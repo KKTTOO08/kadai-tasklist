31eda3243d928b8815c8e5fb38d3179b6fc1221a
KKTTOO <hongtaishankou755@gmail.com>
2021-08-01 10:24:28 +0000

    再提出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,13 +2,16 @@ class TasksController < ApplicationController
     before_action :require_user_logged_in
     before_action :correct_user, only: [:destroy, :update, :edit]
     
-    def index
-        @tasks = Task.all
+  def index
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
     end
+  end
     
-    def show
-        @task = Task.find(params[:id])
-    end
+    #def show
+        #@task = Task.find(params[:id])
+    #end
     
     #toppagesの方で作成
     #def new
